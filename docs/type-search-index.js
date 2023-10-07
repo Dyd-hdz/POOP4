fc1a743a8f5bd067e7bf59a68920e8591e13e20c
Dyd-hdz <146910278+Dyd-hdz@users.noreply.github.com>
2023-10-07 15:05:23 -0600

    Adición de javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"practica04","l":"Alumno"},{"p":"practica04","l":"Coche"},{"p":"practica04","l":"Perro"},{"p":"practica04","l":"Practica04"},{"p":"practica04","l":"Profesor"},{"p":"practica04","l":"Punto"},{"p":"practica04","l":"TrianguloRectangulo"}];updateSearchResults();
